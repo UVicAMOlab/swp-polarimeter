4687b530c1b830a7bcd5706e91bed43071fcb24e
Maxwell <maxwellj.b17@gmail.com>
2023-03-15 16:32:41 -0700

    Created fetch_input_data() helper function to help clean up our large animate_fun() function. Additionally, corrected some naming conventions.

--- polvis.py
+++ polvis.py
@@ -11,12 +11,11 @@ do_save = False
 import swptools as swp
 import numpy as np
 from matplotlib import pyplot as plt, animation
-from mpl_toolkits.mplot3d import axes3d
 import json
 import os.path
 if not run_offline:
-    from daqhats import mcc118, OptionFlags, HatIDs, HatError
-    from daqhats_utils import select_hat_device, enum_mask_to_string, chan_list_to_mask	
+    from daqhats import mcc118, OptionFlags, HatIDs
+    from daqhats_utils import select_hat_device, chan_list_to_mask	
 
 sim_settings_file = 'settings/simsettings.json'
 daq_settings_file = 'settings/daqsettings.json'
@@ -24,69 +23,69 @@ swp_settings_file = 'settings/swpsettings.json'
 
 # Load simulation data from json file
 if run_offline:
-	if not os.path.isfile(sim_settings_file):
-		print(f'Error: simulation file {sim_settings_file} not found.')
-		print('Run \'gen_default_json.py\' to generate default file first.')
-		exit()
-	else:
-		with open(sim_settings_file,'r') as f:
-			simpams = json.load(f)
-			sim_digitize = simpams['sim_digitize']
-			sim_siglevel = simpams['sim_siglevel']
-			sim_ns_level = simpams['sim_ns_level']
-			sim_DOP = simpams['sim_DOP']
-			sim_bg_level = simpams['sim_vbias']
-			sim_wp_phi = simpams['wp_phase']
-			sim_trigger_phase = simpams['sim_phase_offset']
-			sim_poltype = simpams['sim_poltype']
+    if not os.path.isfile(sim_settings_file):
+        print(f'Error: simulation file {sim_settings_file} not found.')
+        print('Run \'gen_default_json.py\' to generate default file first.')
+        exit()
+    else:
+        with open(sim_settings_file,'r') as f:
+            simpams = json.load(f)
+            sim_digitize = simpams['sim_digitize']
+            sim_siglevel = simpams['sim_siglevel']
+            sim_ns_level = simpams['sim_ns_level']
+            sim_DOP = simpams['sim_DOP']
+            sim_bg_level = simpams['sim_vbias']
+            sim_wp_phi = simpams['wp_phase']
+            sim_trigger_phase = simpams['sim_phase_offset']
+            sim_poltype = simpams['sim_poltype']
 
-			if sim_poltype == 'right':
-				S_sim = np.array([1,0,0,1])
-			elif sim_poltype == 'lin':
-				S_sim = np.array([1,1,0,0])
-			else:
-				S_sim = np.array([1,np.sqrt(.3),np.sqrt(.3),np.sqrt(.4)])
+            if sim_poltype == 'right':
+                S_sim = np.array([1,0,0,1])
+            elif sim_poltype == 'lin':
+                S_sim = np.array([1,1,0,0])
+            else:
+                S_sim = np.array([1,np.sqrt(.3),np.sqrt(.3),np.sqrt(.4)])
 
 if not os.path.isfile(daq_settings_file):
-	print(f'Error: simulation file {sim_settings_file} not found.')
-	print('Run \'gen_default_json.py\' to generate default file first.')
-	exit()
+    print(f'Error: simulation file {sim_settings_file} not found.')
+    print('Run \'gen_default_json.py\' to generate default file first.')
+    exit()
 else:
-	with open(daq_settings_file,'r') as f:
-		daqpams = json.load(f)
-		numSamples = daqpams['samples_per_channel']
-		scan_rate = daqpams['scan_rate']
-		channels = daqpams['channels']
-		timeout = daqpams['timeout']
+    with open(daq_settings_file,'r') as f:
+        daqpams = json.load(f)
+        num_samples = daqpams['samples_per_channel']
+        scan_rate = daqpams['scan_rate']
+        channels = daqpams['channels']
+        timeout = daqpams['timeout']
 
-		period = 1/scan_rate
-		totalTime = period*numSamples
-		t = np.linspace(0, totalTime-period, numSamples)
+        period = 1/scan_rate
+        total_time = period*num_samples
+        t = np.linspace(0, total_time-period, num_samples)
 
-		if not run_offline:
-			channel_mask = chan_list_to_mask(channels)
-			num_channels = len(channels)
-			address = select_hat_device(HatIDs.MCC_118)
-			hat = mcc118(address)
-			options = OptionFlags.CONTINUOUS
-            
+        if not run_offline:
+            channel_mask = chan_list_to_mask(channels)
+            num_channels = len(channels)
+            address = select_hat_device(HatIDs.MCC_118)
+            hat = mcc118(address)
+            options = OptionFlags.CONTINUOUS
+
 if not os.path.isfile(swp_settings_file):
-	print(f'Error: settings file {swp_settings_file} not found.')
-	print('Run \'gen_default_json.py\' to generate default file first.')
-	exit()
+    print(f'Error: settings file {swp_settings_file} not found.')
+    print('Run \'gen_default_json.py\' to generate default file first.')
+    exit()
 else:
-	with open(swp_settings_file,'r') as f:
-		swppams = json.load(f)
-		trigger_phase = swppams['trigger_phase']
-		wp_phi = swppams['wp_phi']
-		auto_scale_y_trace = swppams['auto_scale_y_trace']
-		bg_level = swppams['bg_level']
-		data_log_file = swppams['log_data_file']
+    with open(swp_settings_file,'r') as f:
+        swppams = json.load(f)
+        trigger_phase = swppams['trigger_phase']
+        wp_phi = swppams['wp_phi']
+        auto_scale_y_trace = swppams['auto_scale_y_trace']
+        bg_level = swppams['bg_level']
+        data_log_file = swppams['log_data_file']
 
-		if data_log_file != '':
-			do_save = True
-			with open(data_log_file,'w') as f:
-				f.write('')
+        if data_log_file != '':
+            do_save = True
+            with open(data_log_file,'w') as f:
+                f.write('')
 
 
 # Set up plot canvas
@@ -110,7 +109,7 @@ def init_animation():
     ax1.set_title('Polarization Ellipse')
     ax1.grid()
     ax1.set_aspect('equal')
-    
+
     #add unit circle around ellipse
     t_circ = np.linspace(0,2*np.pi,1000)
     x = [np.cos(T) for T in t_circ]
@@ -125,39 +124,42 @@ def init_animation():
     ax2.set_xlabel('Stokes Parameter')
     #ax2.set_ylabel('Value of Stokes Parameter')
     ax2.set_title('Stokes Parameters')
-    
+
     #graph parameters for Trace
     ax3.set_xlim(-1,1000)
     ax3.set_ylim(0,3)
     ax3.set_title('Trace')
     ax3.grid()
-        
+
     return ln1,bar,txt1,ln3,txt_err
-    
-def animate_fun(idx):
-    global phs,t, S_sim
-    estr = 'warnings: '
+
+def fetch_input_data(idx):
+    '''
+    If run_offline, generates simulated input data.
+    If not run_offline, fetches live input data from raspberry pi using daqhats.
+    '''
     if run_offline:
         DP = sim_DOP
         Phi = float(idx/18.)
         w = 2*np.pi*5100/60        
 
         if np.mod(int(idx/50),3) == 0:
-            S_sim = 3*np.array([1, DP*np.cos(Phi)/np.sqrt(2), DP*np.sin(Phi)/np.sqrt(2), DP/np.sqrt(2)])
-            estr = 'ellip-pol. '+estr
+            S_sim = 3*np.array([1, DP*np.cos(Phi)/np.sqrt(2), DP*np.sin(Phi)/np.sqrt(2),                                DP/np.sqrt(2)])
+#            estr = 'ellip-pol. '+estr
         elif np.mod(int(idx/50),3) == 1:            
             S_sim = 3*np.array([1, DP*np.cos(Phi), DP*np.sin(Phi), 0])
-            estr = 'lin-pol. '+estr
+#            estr = 'lin-pol. '+estr
         else:
             S_sim = 3*np.array([1,0,0,DP])
-            estr = 'circ-pol. '+estr
+#            estr = 'circ-pol. '+estr
 
         y1 = swp.simulate_polarization_data(S_sim, w, t, ns_level=sim_ns_level, sig_level=sim_siglevel, digitize_mV=sim_digitize, 
                                             v_bias=sim_bg_level, dphi=sim_wp_phi, ofst=sim_trigger_phase)
         y2 = 5*(np.mod(w*t, 2*np.pi) < np.pi/12)
+
     else:
-        hat.a_in_scan_start(channel_mask, numSamples, scan_rate, options)
-        read_result = hat.a_in_scan_read(numSamples, timeout)
+        hat.a_in_scan_start(channel_mask, num_samples, scan_rate, options)
+        read_result = hat.a_in_scan_read(num_samples, timeout)
 
         # TODO: y1 and y2 need more descriptive name. y1=___Data?? y2=triggerData?
         y1 = abs(np.array(read_result.data[::2])) - bg_level
@@ -167,12 +169,21 @@ def animate_fun(idx):
 
         hat.a_in_scan_stop()
         hat.a_in_scan_cleanup()
+    return y1, y2, idx
 
-    chunkBorderIndices = swp.extract_triggers(y2,TEST = y1)
-    #print(f'Data around trigger: {y1[d-1:d+3:1]}')
+
+# TODO: Find out what idx is and what it does. Any way to avoid it?
+#       seems to only be used when run_offline
+def animate_fun(idx):
+    global phs, t, S_sim
+    estr = 'warnings: '
+
+    y1, y2, idx = fetch_input_data(idx)
 
 # I'm rewriting this part of the algorithm, if only because I'm too stoopid to git it. - AM
-    numChunks = len(chunkBorderIndices)-1
+    chunk_border_indecies = swp.extract_triggers(y2,TEST = y1)
+    #print(f'Data around trigger: {y1[d-1:d+3:1]}')
+    num_chunks = len(chunk_border_indecies)-1
 
     Nroll = 0 # Holds rolling average of pts per chunk (PPC). Used for accuracy warning.
     # For each chunk we calculate the 0,2w, and 4w comonents, averaged over all chunks
@@ -180,19 +191,19 @@ def animate_fun(idx):
 
     S = np.zeros(4)
 
-    for k in range(numChunks):
-        chunk = np.array(y1[chunkBorderIndices[k]:chunkBorderIndices[k+1]])
+    for k in range(num_chunks):
+        chunk = np.array(y1[chunk_border_indecies[k]:chunk_border_indecies[k+1]])
         #get_stokes_from_chunk(cnk,wp_ret = np.pi/2,phs_ofst = 0,verbose = False):
         S += swp.get_stokes_from_chunk(chunk, wp_ret=wp_phi, phs_ofst=trigger_phase, verbose=False)
         Nroll = (Nroll*k + len(chunk))/(k+1) # Update (PPC)
     #computing Stokes Parameters from Fourier Shenanigans (see analysis document)
-    S /= numChunks
+    S /= num_chunks
     S /= S[0]
-    
+
     DOP = np.sqrt(S[1]**2 + S[2]**2 + S[3]**2)
     if do_save: 
-	    with open(data_log_file,'a') as f:
-	    	f.write(f'{S[1]},{S[2]},{S[3]},{DOP}\n')
+        with open(data_log_file,'a') as f:
+            f.write(f'{S[1]},{S[2]},{S[3]},{DOP}\n')
 
 # All kinds of warnings!!!
     if Nroll < 180:
@@ -204,23 +215,23 @@ def animate_fun(idx):
     # if n0 > 4e-2:
     #     # print(f'Warning: Possible alignment error: large cos(2wt) component detected (S,C) = ({b0/nrm},{n0/nrm})')
     #     estr += f'non-zero cos(2w): {round(n0,2)} cf {round(b0*prf/nrm,2)}    '
-        
+
     if np.mean(y1) < 0.08:
         estr += f'Light level too low    '
-        
-    if numChunks < 3:
+
+    if num_chunks < 3:
         estr += f'Insufficient chunks    '
-      
+
     txt_err.set_text(f'({round(np.mean(y1),2)},{round(S[1],2)},{round(S[2],2)},{round(S[3],2)})\n'+estr)
 
-    
+
     txt1.set_text(f'DOP: {round(DOP,3)}')
     if run_offline:
         urr = 100*abs(DOP - sim_DOP)/sim_DOP
         txt2.set_text(f'Error: {round(urr,1)}%')
     else:
         txt2.set_text(f'Mean Signal: {np.mean(y1)}')
-    
+
 # TODO: Why is this different for simulation and for real data? Is there a 
 #       better way to implement simulated data that will require less code?
     for i in range(len(S)):
@@ -228,7 +239,7 @@ def animate_fun(idx):
         if run_offline:
             bar2[i].set_height(S_sim[i]/S_sim[0])
 
-    
+
     x, y = swp.get_polarization_ellipse(S)
     ln1.set_data(x,y)
 
@@ -238,8 +249,8 @@ def animate_fun(idx):
     # if auto_scale_y_trace:
     #     ax3.set_ylim(min(chunk) + 0.001, max(chunk) +0.001)
     # ax3.set_xlim(0, len(chunk))
-    
+
     return ln1, bar, txt1, ln3,
-    
-annie = animation.FuncAnimation(fig,animate_fun,init_func = init_animation, interval = 150)
+
+annie = animation.FuncAnimation(fig, animate_fun, init_func=init_animation, interval=150)
 plt.show()
