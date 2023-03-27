9441683104ec20d09ae846f9bf1e3838df81053e
Maxwell <maxwellj.b17@gmail.com>
2023-03-27 14:49:47 -0700

    Removed run_cal_waveplaye.py as its functionality is entirely present and better implemented in set_wave_plate.py which saves the calculated waveplate retardance into the swp settings file automatically.

--- run_cal_waveplate.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-I_bg = .15
-I_min = .78
-I_max = 2.72
-
-eta = (I_min-I_bg)/(I_max-I_bg)
-sig = 2*eta-1
-print(f'retardance = arccos({round(sig,3)}) = {round(np.arccos(sig),3)}')
